0e7693cf1a24b06d296651b0cb43625e457fea27
Tom Ronen <mystic74@gmail.com>
2018-12-17 20:55:36 +0200

    adding_time_ticks

--- src/main/java/bgu/spl/mics/application/services/TimeService.java
+++ src/main/java/bgu/spl/mics/application/services/TimeService.java
@@ -1,8 +1,13 @@
 package bgu.spl.mics.application.services;
 
+import java.util.TimerTask;
+
+import java.util.Timer;
+
 import bgu.spl.mics.MessageBus;
 import bgu.spl.mics.MessageBusImpl;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.TickBroadcast;
 
 /**
  * TimeService is the global system timer There is only one instance of this micro-service.
@@ -15,53 +20,45 @@ import bgu.spl.mics.MicroService;
  * You MAY change constructor signatures and even add new public constructors.
  */
 
-
-
 public class TimeService extends MicroService{
 	private static class TimeTickHolder{
 		private static TimeService instance= new TimeService();
 	}
+	
 	// The amount of milliseconds between ticks.
 	int tickLength = 0;
 	
 	// How many ticks till death.
 	int amountOfTicks = 0;
 	
-	
+	int currentTick = 0;
 	
 	private TimeService() {
 		super("Time Service");
 		// TODO understand this microservice
 	}
+	
 	public int getTickLength() {
 		return tickLength;
 	}
-
 
 	public void setTickLength(int tickLength) {
 		this.tickLength = tickLength;
 	}
 
-
 	public int getAmountOfTicks() {
 		return amountOfTicks;
 	}
 
-
 	public void setAmountOfTicks(int amountOfTicks) {
 		this.amountOfTicks = amountOfTicks;
 	}
-
-
 	
 	//singleton
 	public static TimeService getInstance()
 	{
 		return TimeTickHolder.instance;
 	}
-	
-	
-
 
 	private int UpdateTicks()
 	{
@@ -72,9 +69,30 @@ public class TimeService extends MicroService{
 	protected void initialize() {
 		
 		MessageBusImpl.getInstance().register(this);
-		// TODO Auto-generated method stub
 		
-		// We should set a timer that counts back ticks on the amount of time delegated by the input.
+		if (this.amountOfTicks > this.currentTick) 
+		{
+			// We should set a timer that counts back ticks on the amount of time delegated by the input.
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+				
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					TickBroadcast tB = new TickBroadcast();
+					tB.setTick(currentTick);
+					sendBroadcast(tB);
+					currentTick++;
+					initialize();
+				}
+			}, this.tickLength);
+		}
+		else
+		{
+			// Die bitch.
+			
+			this.terminate();
+		}
 	}
 
 }
